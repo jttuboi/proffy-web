3ffa883f2e61fb13ce921d10f53cd64263b2b83a
jttuboi <tuboi.jairo@gmail.com>
2020-08-03 14:45:09 -0300

    add content to teacherlist.

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import './styles.css';
+import WhatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
 
 function TeacherList() {
     return (
@@ -10,18 +12,47 @@ function TeacherList() {
                 <form id="search-teachers">
                     <div className="input-block">
                         <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
+                        <input type="text" id="subject" />
                     </div>
                     <div className="input-block">
                         <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="week_day"/>
+                        <input type="text" id="week_day" />
                     </div>
                     <div className="input-block">
                         <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
+                        <input type="text" id="time" />
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars1.githubusercontent.com/u/376856?s=460&u=a72e6c5e00639eaed8aad35b597a81e71860cbb5&v=4" alt="Jairo Tuboi" />
+                        <div>
+                            <strong>Jairo Tuboi</strong>
+                            <span>Desenvolvedor de software</span>
+                        </div>
+                    </header>
+                    <p>
+                        Bla blabla blablabla bla blabla bla blablabla bla bla bla.
+                        Bla blabla blablabla bla blabla bla blablabla bla bla bla.
+                        <br />
+                        <br />
+                        Bla blabla blablabla bla blabla bla blablabla bla bla bla.
+                    </p>
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={WhatsappIcon} alt="Whatsapp"/>
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
         </div>
     );
 }
