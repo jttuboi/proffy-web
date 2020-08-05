36ed6965aa505dbe08a6fcb0dee0cf3d194a907e
jttuboi <tuboi.jairo@gmail.com>
2020-08-05 15:22:41 -0300

    add select and textarea components

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Textarea from '../../components/Textarea';
 import PageHeader from '../../components/PageHeader';
 
 import WarningIcon from '../../assets/images/icons/warning.svg';
@@ -18,15 +20,29 @@ function TeacherForm() {
              <main>
                 <fieldset>
                     <legend>Seus dados</legend>
-                    
                     <Input name="name" label="Nome Completo" />
                     <Input name="avatar" label="Avatar" />
                     <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografia" />
                 </fieldset>
                 <fieldset>
                     <legend>Sobre a aula</legend>
-                    
-                    <Input name="subject" label="Matéria" />
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'Educação física', label: 'Educação física' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Química', label: 'Química' }
+                        ]}
+                     />
                     <Input name="cost" label="Custo da sua hora por aula" />
                 </fieldset>
 
