8a3cb5e4239e847d3d778f359340c662d205d2d2
jttuboi <tuboi.jairo@gmail.com>
2020-08-05 14:41:24 -0300

    add teacherform inputs. create input component.

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -10,6 +10,7 @@ import './styles.css';
 /* Properties */
 interface PageHeaderProps {
     title: string;
+    description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -17,12 +18,13 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
         <header className="page-header">
             <div className="top-bar-container">
                 <Link to="/">
-                    <img src={BackIcon} alt="Voltar"/>
+                    <img src={BackIcon} alt="Voltar" />
                 </Link>
-                <img src={LogoImg} alt="Proffy"/>
+                <img src={LogoImg} alt="Proffy" />
             </div>
             <div className="header-content">
                 <strong>{props.title}</strong>
+                {props.description && <p>{props.description}</p>}
                 {props.children}
             </div>
         </header>
